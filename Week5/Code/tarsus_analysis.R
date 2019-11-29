00c1316e930618b576e0e61f7570c1e242eeca47
Sam Turner <samturner0306@gmail.com>
2019-11-29 15:52:40 +0000

    Week 7 work

---
+++ Week5/Code/tarsus_analysis.R
@@ -0,0 +1,8 @@
+setwd("../")
+
+tarsus <- read.table("tarsus.csv", sep = ',', header = T)
+
+b = seq(min(tarsus$Tarsus) - 1, max(tarsus$Tarsus) + 1, 0.5)
+
+hist(tarsus$Tarsus, breaks = b)
+head(tarsus)
