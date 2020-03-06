fe5aad076418cd3431961209718306f55292e97d
Sam Turner <samturner0306@gmail.com>
2020-03-06 08:07:25 +0000

    Final final push

--- MP/code/run_MiniProject.sh
+++ MP/code/run_MiniProject.sh
@@ -1,6 +1,5 @@
 
 printf "Preparing data...\n"
-
 Rscript "data_preparation.R"
 
 printf "Calculating initial values...\n"
@@ -9,8 +8,14 @@ python3 "initial_vals.py"
 printf "Making demonstration plots...\n"
 Rscript "demo_plots.R"
 
+
 printf "Performing analysis...\n"
 Rscript "fitting_and_analysis.R"
 
 printf "Compiling report...\n"
 bash "CompileLaTeX.sh"
+
+rm "Rplots.pdf"
+rm "*.sum"
+rm ".write_up.pdf"
+rm ".Rhistory"
